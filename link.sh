075d55d864de60bbbf19645e5bc9b676975abda8
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-03-21 15:25:44 +0900

    Add shell script

---
+++ link.sh
@@ -0,0 +1,74 @@
+#!/bin/zsh
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    link.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/03/21 14:30:25 by smun              #+#    #+#              #
+#    Updated: 2022/03/21 14:32:37 by smun             ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Prefix path used by symbolic linking.
+
+export HOME_PREFIX=~
+export PREFIX=~/goinfre
+
+function copy_and_merge() {
+	SRCPATH=$1
+	DESTPATH=$2
+
+	echo "Copying.."
+	cp -af "$SRCPATH/" "$DESTPATH/"
+}
+
+function link() {
+	LINKNAME=$1
+	SRCPATH="$HOME_PREFIX"'/'"$LINKNAME"
+	DESTPATH="$PREFIX"'/'"$LINKNAME"
+
+	if [ ! -d "$SRCPATH" ]
+	then
+		echo "Not exists link name: $LINKNAME ... skipping.."
+		return
+	fi
+
+	printf "[%s] \n" "$LINKNAME"
+
+	# Create directory if not exists
+	mkdir -p "$DESTPATH" 2>&1 > /dev/null
+
+	# Copy and merging if any caches already exist.
+	copy_and_merge "$SRCPATH" "$DESTPATH"
+
+	echo "Linking.."
+
+	# Remove previous path and make a symbolic link with ~/goinfre
+	rm -rf "$SRCPATH"
+	ln -s "$DESTPATH" "$SRCPATH"
+	echo "Done!"
+}
+
+link "Library/Caches/vscode-cpptools"
+link "Library/Application Support/Slack/Cache"
+link "Library/Application Support/Code/Cache"
+link "Library/Application Support/Code/Code Cache"
+link "Library/Application Support/Code/CachedData"
+link "Library/Application Support/Code/CachedExtensionVSIXs"
+link "Library/Application Support/Code/CachedExtensions"
+link "Library/Application Support/Code/User"
+link "Library/Application Support/discord"
+link "Library/Application Support/Caches"
+link "Library/Developer/CoreSimulator"
+link "Library/Caches/com.apple.Music"
+link "Library/Caches/com.tinyspeck.slackmacgap"
+link "Library/Caches/com.tinyspeck.slackmacgap.ShipIt"
+link "Library/Caches/com.hnc.Discord"
+link "Library/Caches/com.hnc.Discord.ShipIt"
+link "Library/Caches/Google"
+link "Library/Caches/com.microsoft.VSCode"
+link "Library/Caches/com.microsoft.VSCode.ShipIt"
+link "Library/Caches/Firefox"
